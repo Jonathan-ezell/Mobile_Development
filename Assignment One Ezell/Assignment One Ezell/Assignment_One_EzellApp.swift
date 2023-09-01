3ab9afcb817ec415d9cd6e354a11b75f3a2bdefc
Jonathan-ezell <143762976+Jonathan-ezell@users.noreply.github.com>
2023-09-01 16:35:23 -0600

    Add files via upload

---
+++ Assignment One Ezell/Assignment One Ezell/Assignment_One_EzellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_One_EzellApp.swift
+//  Assignment One Ezell
+//
+//  Created by Jon Ezell on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_One_EzellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
