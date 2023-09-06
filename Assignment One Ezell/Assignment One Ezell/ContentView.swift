4f43f08cbc88c5725e59eb53fb95c001f8409783
Jonathan-ezell <143762976+Jonathan-ezell@users.noreply.github.com>
2023-09-06 11:43:13 -0600

    Add files via upload

--- Assignment One Ezell/Assignment One Ezell/ContentView.swift
+++ Assignment One Ezell/Assignment One Ezell/ContentView.swift
@@ -4,23 +4,40 @@
 //
 //  Created by Jon Ezell on 8/31/23.
 //
-
+//import UIkit
 import SwiftUI
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .top) {
+            Image("chautauqua_flatirons") // Replace with your image name
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .edgesIgnoringSafeArea(.all)
+            
+            Spacer()
+            // Add other SwiftUI views on top of the background image
+            Text("I am Starting to figure out the basics!")
+                .font(.largeTitle)
+                .foregroundColor(.black)
+                .bold()
+                .padding()
+                .padding(.top, 120)
+                              }
+            Spacer()
         }
-        .padding()
+    }
+
+
+
+struct YourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
